b325602903c76e8e77cb086531b1842fd0f8850c
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-06-17 17:22:17 +0200

    feat: milestone 0

---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,66 @@
+import { Children, createContext, useEffect, useState } from "react";
+
+const GlobalContext = createContext();
+
+const GlobalProvider = ({children}) => {
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState([]);
+  const [searchTermSubmit, setSearchTermSubmit] = useState("");
+
+  const apiKey = import.meta.env.VITE_APP_KEY;
+
+  const fetchResult = () => {
+    if (!search.trim()) return;
+
+    const apiMovieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=it-IT&query=${searchTerm}`;
+
+    const apiTvSeriesUrl = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&language=it-IT&query=${searchTerm}`;
+
+    axios.get(apiMovieUrl).then((movieResp) => {
+      const movies = movieResp.data.results.map((item) => ({
+        id: item.id,
+        title: item.title,
+        originalTitle: item.original_title,
+        originalTitle: item.original_title,
+        language: item.original_language,
+        vote: item.vote_average,
+        poster: item.poster_path,
+        type: "movie",
+      }));
+
+      axios.get(apiTvSeriesUrl).then((tvSeriesResp) => {
+        const tvSeries = tvSeriesResp.data.results.map((item) => ({
+          id: item.id,
+          title: item.name,
+          originalTitle: item.original_name,
+          language: item.original_language,
+          vote: item.vote_average,
+          poster: item.poster_path,
+          type: "tv",
+        }));
+
+        setResult([...movies, ...tvSeries]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    if (!searchTermSubmit.trim()) return;
+    fetchResult();
+  }, [searchTermSubmit]);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        search,
+        setSearch,
+        setSearchTermSubmit,
+        result,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export {GlobalContext, GlobalProvider};
